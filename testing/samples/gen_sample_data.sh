8a3cf3dd532f3edbba625e5339727a4ce0d27089
Gary Niu <gary.niu@gmail.com>
2017-02-23 23:33:07 +0800

    update

---
+++ testing/samples/gen_sample_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dd if=/dev/zero of=$1 bs=$1 count=1
+
--- wlst/common.py
+++ wlst/common.py
@@ -28,18 +28,19 @@ cluster_name = 'MyCluster'
 jms_res={'module_name':'MyJMSResources', 'cf_name':'MyCF', 'dqueue_name':'MyDQ', 'cf_jndi':'mycf', 'dqueue_jndi':'mydq'}
 jms_servers = {'JMSServer':'FileStore',}
 
-
+'''
 # for testing start
 file_stores = {'FileStore':'/Users/gniu/Temp.localized/store',}
 domain = Domain('/Users/gniu/Oracle/mw12c/user_projects/domains', 'jms_domain')
 machines = (Machine('node1', 'localhost', {'MS1_1':7101, 'MS1_2':7102}),)
 # for testing end
-
 '''
+
 file_stores = {'FileStore':'/u01/oracle/temp/store',}
 domain = Domain('/u01/oracle/mw12c/user_projects/domains', 'jms_domain')
-machines = (Machine('node1', '192.168.10.101', {'MS1_1':7101, 'MS1_2':7102}), Machine('node3', '192.168.10.103', {'MS3_1':7301, 'MS3_2':7302}), Machine('node4', '192.168.10.104', {'MS4_1':7401, 'MS4_2':7402}), Machine('node5', '192.168.10.105', {'MS5_1':7501, 'MS5_2':7502}))
-'''
+#machines = (Machine('node1', '192.168.10.101', {'MS1_1':7101, 'MS1_2':7102}), Machine('node3', '192.168.10.103', {'MS3_1':7301, 'MS3_2':7302}), Machine('node4', '192.168.10.104', {'MS4_1':7401, 'MS4_2':7402}), Machine('node7', '192.168.10.107', {'MS7_1':7701, 'MS7_2':7702}))
+machines = (Machine('node3', '192.168.10.103', {'MS3_1':7301, 'MS3_2':7302}), Machine('node4', '192.168.10.104', {'MS4_1':7401, 'MS4_2':7402}))
+
 
 def message_box(msg):
     print ''
